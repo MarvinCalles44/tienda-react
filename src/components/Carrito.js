d34602888bb053ffc203bc5f0599a0e0c5befc02
Marvin Calles <marvincalles02@gamil.com>
2022-07-22 19:43:02 -0600

    Primer Commit

---
+++ src/components/Carrito.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './carrito.css';
+import Producto from './Producto';
+
+
+const Carrito = ({carrito, agregarProducto}) => (
+
+    <div className='carrito'>
+    <h2>Carrito de Compras</h2>
+
+    {carrito.length === 0 ? <p>Esta vacio el</p>
+
+    :carrito.map(producto =>(
+        
+        <Producto
+            key={producto.id}
+            producto={producto}
+            carrito={carrito}
+            agregarProducto={agregarProducto}
+        />
+    ))}
+
+    </div>
+    )
+
+export default Carrito
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = ({fecha}) => {
+    
+    return(
+        <footer>
+            <p>Todos los derechos reservados {fecha} ©</p>
+        </footer>
+    )
+}
+
+export default Footer;
